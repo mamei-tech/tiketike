d9c2a01f71768f4fb541e677db479abd4e29ad86
roniel <ronielsh6@gmail.com>
2018-11-07 21:24:56 -0600

    probed testing

--- resources/views/userprofilefront.blade.php
+++ resources/views/userprofilefront.blade.php
@@ -1,58 +1,58 @@
-@extends('admin.layouts.base')
+{{--@extends('admin.layouts.base')--}}
 
-@section('adminview', __('aUserprofile.sectiontitle') )
+{{--@section('adminview', __('aUserprofile.sectiontitle') )--}}
 
-@section('pageheader')
-    @component('admin.components.pageheader')
-        @slot('classSpec')
-            panel-header-sm
-        @endslot
+{{--@section('pageheader')--}}
+    {{--@component('admin.components.pageheader')--}}
+        {{--@slot('classSpec')--}}
+            {{--panel-header-sm--}}
+        {{--@endslot--}}
 
-    @endcomponent
-@endsection
+    {{--@endcomponent--}}
+{{--@endsection--}}
 
-@section('content')
+{{--@section('content')--}}
 
-    <div class="row">
+    {{--<div class="row">--}}
 
         {{-- USER FORM --}}
-        <div class="col-md-8">
-            <div class="card">
+        {{--<div class="col-md-8">--}}
+            {{--<div class="card">--}}
 
                 {{-- TODO Apply this style tittle to the other views --}}
-                <div class="card-header">
-                    <h5 class="title"> User </h5>
-                </div>
+                {{--<div class="card-header">--}}
+                    {{--<h5 class="title"> User </h5>--}}
+                {{--</div>--}}
 
-                <div id="div_formContainer" class="card-body">
-                    @include('admin.partials.forms.userprofile_form')
-                </div>
+                {{--<div id="div_formContainer" class="card-body">--}}
+                    {{--@include('admin.partials.forms.userprofile_form')--}}
+                {{--</div>--}}
 
-            </div>
-        </div>
+            {{--</div>--}}
+        {{--</div>--}}
 
         {{-- USER CARD --}}
-        <div class="col-md-4">
-            <div class="card-container">
-                <div class="rotating-card">
+        {{--<div class="col-md-4">--}}
+            {{--<div class="card-container">--}}
+                {{--<div class="rotating-card">--}}
 
                     {{-- FRONT --}}
-                    @include('admin.partials.cards.usercard-front')
+                    {{--@include('admin.partials.cards.usercard-front')--}}
 
                     {{-- BACK --}}
-                    @include('admin.partials.cards.usercard-back')
+                    {{--@include('admin.partials.cards.usercard-back')--}}
 
-                </div>
-            </div>
-        </div>
-    </div>
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 
 
-@endsection
+{{--@endsection--}}
 
-@section('footerScripts')
-    @parent
-    <script src="{{ asset('js/admin/admin_views.js') }}" defer></script>
-    <script src="{{ asset('js/admin/userprofile.js') }}" defer></script>
-@endsection
+{{--@section('footerScripts')--}}
+    {{--@parent--}}
+    {{--<script src="{{ asset('js/admin/admin_views.js') }}" defer></script>--}}
+    {{--<script src="{{ asset('js/admin/userprofile.js') }}" defer></script>--}}
+{{--@endsection--}}
 
