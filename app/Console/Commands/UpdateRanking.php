5c3e38cc021016262283351ab6b5c1f40fe0a3f0
Alejandro Cuevas <acuevas1605@gmail.com>
2018-12-05 14:29:52 -0500

    This is my first commit from windows

--- app/Console/Commands/UpdateRanking.php
+++ app/Console/Commands/UpdateRanking.php
@@ -40,24 +40,24 @@ class UpdateRanking extends Command
     public function handle()
     {
 
-            $users = User::all();
-            foreach ($users as $user)
-            {
-                $created_raffles = count($user->getRaffles);
-                $buyed_tickets = count($user->getTickets);
-                $tickets = User::with('getTickets')
-                    ->whereHas('getTickets', function (Builder $q) use ($user){
-                        $q->where('bingo',0);
-                        $q->where('buyer',$user->id);
-                    })
-                    ->get();
-                $times_winner = count($tickets);
-                $raferals = count($user->getReferralsBuys);
-                $comments = count($user->getComments);
-                $ranking = $created_raffles*0.3 + $buyed_tickets*0.05 + $times_winner*0.3 + $raferals*0.3 + $comments*0.05;
-                $user->ranking = $ranking;
-                $user->save();
-            }
+        $users = User::all();
+        foreach ($users as $user)
+        {
+            $created_raffles = count($user->getRaffles);
+            $buyed_tickets = count($user->getTickets);
+            $tickets = User::with('getTickets')
+                ->whereHas('getTickets', function (Builder $q) use ($user){
+                    $q->where('bingo',0);
+                    $q->where('buyer',$user->id);
+                })
+                ->get();
+            $times_winner = count($tickets);
+            $raferals = count($user->getReferralsBuys);
+            $comments = count($user->getComments);
+            $ranking = $created_raffles*0.3 + $buyed_tickets*0.05 + $times_winner*0.3 + $raferals*0.3 + $comments*0.05;
+            $user->ranking = $ranking;
+            $user->save();
+        }
 
     }
 }
